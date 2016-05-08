52222beb7b2b94e9e0502ba199500a62773d3cd1
robot3d <56633608@qq.com>
2016-05-08 14:13:30 +0800

    51zxw init
    基础学习

---
+++ 51zxw/Assets/test001/test001.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+/**
+用Instantiat复制Prefab 
+*/
+function Start () {
+
+}
+var source:GameObject;
+function Update () {
+   var go:GameObject = Instantiate(source,transform.position,transform.rotation);
+
+}
