407d17cc90bce22dec33dde8a595cb289e9c9502
robot3d <56633608@qq.com>
2016-05-11 14:23:17 +0800

    打包加载条目

--- 51zxw/Assets/scripts/main.cs
+++ 51zxw/Assets/scripts/main.cs
@@ -16,10 +16,13 @@ public class main : MonoBehaviour {
 	}
 	void OnGUI(){
 		GUI.Label(new Rect(10,20,Screen.width,30),"select:"); 
-		if (GUI.Button (new Rect (80, 20, 60, 30), "scene 1")) {
+		if (GUI.Button (new Rect (80, 20, 60, 30), "创建物体")) {
 			Application.LoadLevel(1);
 		}
-		if (GUI.Button (new Rect (80, 60, 60, 30), "scene 2")) {
+		if (GUI.Button (new Rect (80, 60, 60, 30), "基础综合")) {
+			Application.LoadLevel(2);
+		}
+		if (GUI.Button (new Rect (80, 100, 60, 30), "打包加载")) {
 			Application.LoadLevel(2);
 		}
 	}
